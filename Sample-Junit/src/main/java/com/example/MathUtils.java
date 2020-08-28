12c863cab2d87fd664b55674024ddcd6bd38b565
Deepankar Pathak <vp3008@gmail.com>
2020-08-29 04:36:00 +0530

    Ran simple test cases

--- Sample-Junit/src/main/java/com/example/MathUtils.java
+++ Sample-Junit/src/main/java/com/example/MathUtils.java
@@ -1,9 +1,12 @@
 package com.example;
 
 public class MathUtils {
-	
+
 	public int add(int a, int b) {
 		return a + b;
 	}
 
+	public double computeCircleArea(double radius) {
+		return Math.PI * radius * radius;
+	}
 }
--- Sample-Junit/src/test/java/com/example/MathUtilsTest.java
+++ Sample-Junit/src/test/java/com/example/MathUtilsTest.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 //import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
@@ -7,7 +9,17 @@ import org.junit.jupiter.api.Test;
 class MathUtilsTest {
 
 	@Test
-	void test() {
-		System.out.println("this test ran");
+	void testAdd() {
+		MathUtils mathUtils= new MathUtils();
+		int expected = 5;
+		int actual = mathUtils.add(3, 2);
+		assertEquals(expected, actual, "The add method should add 2 numbers");
+	}
+	
+	
+	@Test
+	void testComputeCircleRadius() {
+		MathUtils mathUtils = new MathUtils();
+		assertEquals(314.1592653589793, mathUtils.computeCircleArea(10), "Should return right circle area");
 	}
 }
