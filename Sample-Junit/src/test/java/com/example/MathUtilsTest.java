09005ea31bf4bb3379df071777fb2cdb2aae3db6
Deepankar Pathak <vp3008@gmail.com>
2020-08-29 05:21:44 +0530

    Hook annotations- beforeEach

--- Sample-Junit/src/test/java/com/example/MathUtilsTest.java
+++ Sample-Junit/src/test/java/com/example/MathUtilsTest.java
@@ -1,25 +1,45 @@
 package com.example;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
 
 //import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
 class MathUtilsTest {
+	
+	MathUtils mathUtils;
+	
+	@BeforeEach
+	void init() {
+		mathUtils = new MathUtils();
+	}
 
 	@Test
 	void testAdd() {
-		MathUtils mathUtils= new MathUtils();
 		int expected = 5;
 		int actual = mathUtils.add(3, 2);
 		assertEquals(expected, actual, "The add method should add 2 numbers");
 	}
-	
-	
+
+	@Test
+	void testDivide() {
+		assertThrows(ArithmeticException.class, () -> mathUtils.divide(1, 0), "Divide by 0 should throw");// fails when
+																											// the wrong
+																											// exception
+																											// type is
+																											// specified
+																											// or if
+																											// there is
+																											// no
+																											// exception
+	}
+
 	@Test
 	void testComputeCircleRadius() {
-		MathUtils mathUtils = new MathUtils();
 		assertEquals(314.1592653589793, mathUtils.computeCircleArea(10), "Should return right circle area");
 	}
 }
