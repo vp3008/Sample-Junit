98c40b5558ec43ae8e678a9065ca17ca6bab62cb
Deepankar Pathak <vp3008@gmail.com>
2020-08-29 04:10:30 +0530

    Fixed Junit test not running

---
+++ Sample-Junit/src/main/java/com/example/MathUtils.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class MathUtils {
+	
+	public int add(int a, int b) {
+		return a + b;
+	}
+
+}
---
+++ Sample-Junit/src/test/java/com/example/MathUtilsTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+//import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MathUtilsTest {
+
+	@Test
+	void test() {
+		System.out.println("this test ran");
+	}
+}
